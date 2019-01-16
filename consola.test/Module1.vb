9adef8f7d26c1864c0b8f941964840b25e4f130b
Albis Oro <programando.oro@gmail.com>
2019-01-16 16:34:23 -0500

    inicio de proyecto dll y consola de prueba

---
+++ consola.test/Module1.vb
@@ -0,0 +1,16 @@
+﻿Imports System
+Imports data = [lib].test
+
+Module Module1
+
+    Sub Main()
+        Dim sResult As String
+        Dim oData As New data.Class1
+
+        sResult = oData.Connection()
+
+        Console.WriteLine("Result : " + sResult)
+        Console.ReadKey()
+    End Sub
+
+End Module
